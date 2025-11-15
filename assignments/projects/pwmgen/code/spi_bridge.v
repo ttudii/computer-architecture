97a67c29c8100df43fbaeb92b14c8a3ffc31ff3c
Tudor Cinteza <tudor.cinteza@gmail.com>
2025-11-15 18:46:17 +0200

    implemented outside signals sclk and cs synchronization with internal clock

--- assignments/projects/pwmgen/code/spi_bridge.v
+++ assignments/projects/pwmgen/code/spi_bridge.v
@@ -13,4 +13,24 @@ module spi_bridge (
     input[7:0] data_out
 );
 
+reg [1:0] sclk_sync, cs_sync;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        sclk_sync <= 2'b00;
+        cs_sync <= 2'b11;
+    end else begin
+        sclk_sync <= {sclk_sync[0], sclk}
+        cs_sync <= {cs_sync[0], cs_n}
+    end
+end
+
+wire sclk_rising = (sclk_sync == 2'b01);
+wire sclk_falling = (sclk_sync == 2'b10);
+wire cs_active = ~cs_sync[1];
+
+// Shift Registers and Counters
+
+// SPI Logic
+
 endmodule
