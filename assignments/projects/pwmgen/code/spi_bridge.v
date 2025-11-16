d38e785ef9cee8b8d006fc41265e5de1576ff83a
Tudor Cinteza <tudor.cinteza@gmail.com>
2025-11-16 15:43:15 +0200

    fixed syntax errors and added new functionality: reload of transmit register between bytes.

--- assignments/projects/pwmgen/code/spi_bridge.v
+++ assignments/projects/pwmgen/code/spi_bridge.v
@@ -20,8 +20,8 @@ always @(posedge clk or negedge rst_n) begin
         sclk_sync <= 2'b00;
         cs_sync <= 2'b11;
     end else begin
-        sclk_sync <= {sclk_sync[0], sclk}
-        cs_sync <= {cs_sync[0], cs_n}
+        sclk_sync <= {sclk_sync[0], sclk};
+        cs_sync <= {cs_sync[0], cs_n};
     end
 end
 
@@ -33,32 +33,38 @@ wire cs_active = ~cs_sync[1];
 reg [2:0] byte_cnt;
 reg [7:0] shift_in, shift_out;
 reg miso_reg;
-assign miso <= miso_reg
-assign data_in <= shift_in
+assign miso = miso_reg;
+assign data_in = shift_in;
 
 // SPI Logic
 always @(posedge clk or negedge rst_n) begin
     if(!rst_n) begin
-        byte_cnt <= 3d'0;
-        shift_in <= 8d'0;
-        shift_out <= 8d'0;
-        miso_reg <= 1b'0;
+        byte_cnt <= 3'd0;
+        shift_in <= 8'd0;
+        shift_out <= 8'd0;
+        miso_reg <= 1'b0;
     end else begin
         if (!cs_active) begin
-            byte_cnt <= 3d'0;
+            byte_cnt <= 3'd0;
             shift_out <= data_out;
             miso_reg <= data_out[7];
         end else begin
             //reading data
             if(sclk_rising) begin
-                shift_in <= {shift_in[6], mosi};
+                shift_in <= {shift_in[6:0], mosi};
                 byte_cnt <= byte_cnt + 1;    
             end
 
             //sending data
             if(sclk_falling) begin
-                shift_out <= {shift_out[6], 1'b0};
+                shift_out <= {shift_out[6:0], 1'b0};
                 miso_reg <= shift_out[6];
+            end
+
+            //reload TX register between bytes
+            if (sclk_rising && (byte_cnt == 3'd7)) begin
+                shift_out <= data_out;  // load next TX byte
+                miso_reg  <= data_out[7];
             end
         end
     end
@@ -72,7 +78,7 @@ always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
         byte_sync_reg <= 1'b0;
     else
-        byte_sync_reg <= cs_active && sclk_rising && (bit_cnt == 3'd7);
+        byte_sync_reg <= cs_active && sclk_rising && (byte_cnt == 3'd7);
 end
 
 endmodule
